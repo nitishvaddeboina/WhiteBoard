5dd2948c3b4357de7536d319a1d81f090b285f0c
Nitish <nitishvaddeboina@gmail.com>
2024-12-09 20:05:16 -0600

    intial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Board from './component/Board';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Board from "./component/Board";
 
 const CanvasDrawing = () => {
-
-  const [brushColor, setBrushColor] = useState('black');
+  const [brushColor, setBrushColor] = useState("black");
   const [brushSize, setBrushSize] = useState<number>(5);
 
   useEffect(() => {
@@ -12,11 +11,11 @@ const CanvasDrawing = () => {
   }, [brushSize]);
 
   return (
-    <div className="App" >
+    <div className="App">
       <h1>Collaborative Whiteboard</h1>
       <div>
         <Board brushColor={brushColor} brushSize={brushSize} />
-        <div className='tools' >
+        {/* <div className='tools' >
           <div>
             <span>Color: </span>
             <input type="color" value={brushColor} onChange={(e) => setBrushColor(e.target.value)} />
@@ -27,7 +26,7 @@ const CanvasDrawing = () => {
               min="1" max="100" value={brushSize} onChange={(e) => setBrushSize(Number(e.target.value))} />
             <span>{brushSize}</span>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from any network interface
+    port: 3001      // Use a specific port
+  }
 })
+
